0aa13f9096c715450a074d986ac5543ba65dbcb2
siegengin <109764860+siegengin@users.noreply.github.com>
2023-09-11 13:34:06 -0400

    fixed compile error

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -1,6 +1,36 @@
-class BinaryFormatException extends Exception {
-    public BinaryFormatException(String message) {
-        super(message);
+import java.util.Scanner;
+
+public class BinaryConverter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a binary number as a string: ");
+        String binaryString = input.nextLine();
+
+        try {
+            int decimalValue = bin2Dec(binaryString);
+            System.out.println("Decimal equivalent: " + decimalValue);
+        } catch (BinaryFormatException e) {
+            System.out.println("Not a binary number.");
+        }
+
+        input.close();
+    }
+
+    public static int bin2Dec(String binaryString) throws BinaryFormatException {
+        int decimalValue = 0;
+        int length = binaryString.length();
+
+        for (int i = 0; i < length; i++) {
+            char c = binaryString.charAt(i);
+            if (c != '0' && c != '1') {
+                throw new BinaryFormatException("The string is not a binary string");
+            }
+
+            int digit = Character.getNumericValue(c);
+            decimalValue += digit * Math.pow(2, length - 1 - i);
+        }
+
+        return decimalValue;
     }
 }
-
